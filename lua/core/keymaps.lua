ec7331b40910d70c5b3795701cb2a5526c9fdd80
porterrigby <prigby.a@gmail.com>
2023-11-22 08:24:31 -0700

    added keybinds and settings.lua file for setting nvim options

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,10 +1,4 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
-
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
-
 vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
-
-
---
+++ lua/core/settings.lua
@@ -0,0 +1,11 @@
+-- lines and stuff
+vim.opt.wrap = false
+vim.opt.number = true
+vim.opt.colorcolumn = "0"
+
+-- tabs and stuff
+vim.opt.smarttab = true
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+
