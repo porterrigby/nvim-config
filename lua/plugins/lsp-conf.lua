b9fe80ffbc65fe65f0c8698cae74b68aa5391569
porterrigby <prigby.a@gmail.com>
2024-07-22 15:34:10 -0600

    Added formatting and linting for js and ts and python.

--- lua/plugins/lsp-conf.lua
+++ lua/plugins/lsp-conf.lua
@@ -1,57 +1,57 @@
 return {
-    {
-        "williamboman/mason.nvim",
-        config = function()
-            require("mason").setup()
-        end
-    },
-    {
-        "williamboman/mason-lspconfig.nvim",
-        config = function()
-            require("mason-lspconfig").setup({
-            -- Lsp to install
-            ensure_installed = {"lua_ls", "clangd", "pyre", "jdtls", "tsserver"}
-            })
-        end
-    },
-    {
-        "neovim/nvim-lspconfig",
-        config = function()
-            local capabilities = require("cmp_nvim_lsp").default_capabilities()
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				-- Lsp to install
+				ensure_installed = { "lua_ls", "clangd", "pyre", "jdtls", "tsserver" },
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-            -- Set up each language server
-            local lspconfig = require("lspconfig")
-            lspconfig.lua_ls.setup({
-                capabilities = capabilities
-            })
-            lspconfig.clangd.setup({
-                capabilities = capabilities
-            })
-            lspconfig.pyre.setup({
-                capabilities = capabilities
-            })
---[[            lspconfig.jdtls.setup({     -- currently handled via nvim-jdtls?
+			-- Set up each language server
+			local lspconfig = require("lspconfig")
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.clangd.setup({
+				capabilities = capabilities,
+			})
+			lspconfig.pyre.setup({
+				capabilities = capabilities,
+			})
+			--[[            lspconfig.jdtls.setup({     -- currently handled via nvim-jdtls?
                 capabilities = capabilities
             }) --]]
-            lspconfig.tsserver.setup({
-                capabilities = capabilities,
-                autostart = true,
-                filetypes = {
-                    "javascript",
-                    "javascriptreact",
-                    "javascript.jsx",
-                    "typescript",
-                    "typescriptreact",
-                    "typescript.tsx",
-                },
-            })
-            lspconfig.html.setup({
-                capabilities = capabilities
-            })
+			lspconfig.tsserver.setup({
+				capabilities = capabilities,
+				autostart = true,
+				filetypes = {
+					"javascript",
+					"javascriptreact",
+					"javascript.jsx",
+					"typescript",
+					"typescriptreact",
+					"typescript.tsx",
+				},
+			})
+			lspconfig.html.setup({
+				capabilities = capabilities,
+			})
 
-            vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-            vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-            vim.keymap.set({"n", "v"}, "<leader>ca", vim.lsp.buf.code_action, {})
-        end
-    }
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+		end,
+	},
 }
